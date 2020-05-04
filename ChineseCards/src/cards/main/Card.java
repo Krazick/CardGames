27fe4fbfd9a7b469d259d93ad05c17cdd27149c4
Mark Smith <markjsmith60@gmail.com>
2020-05-04 09:58:31 -0400

    Added Suit ENUMs

--- ChineseCards/src/cards/main/Card.java
+++ ChineseCards/src/cards/main/Card.java
@@ -1,15 +1,46 @@
 package cards.main;
 
 public class Card {
-	public static int MIN_RANK = 1;
-	public static int MAX_RANK = 13;
+	public static int MIN_RANK_INDEX = 1;
+	public static int MAX_RANK_INDEX = 13;
 	public Card NO_CARD = null;
 	
+	public enum Suit { 
+		NO_SUIT  ("INVALID",  "NO COLOR", "SYMBOL"),
+		HEARTS   ("Hearts",   "Red",      "H"),
+		DIAMONDS ("Diamonds", "Red",      "D"),
+		CLUBS    ("Clubs",    "Black",    "C"),
+		SPADES   ("Spades",   "Black",    "B");
+		
+		String name, color, symbol;
+		Suit (String aName, String aColor, String aSymbol) {
+			name = aName;
+			color = aColor;
+			symbol = aSymbol;
+		}
+		
+		public String getName () {
+			return name;
+		}
+		
+		public String toString () {
+			return name;
+		}
+		
+		public String getColor () {
+			return color;
+		}
+		
+		public String getSymbol () {
+			return symbol;
+		}
+	}
+	
 	public enum Rank { NO_CARD ("INVALID", 0),
-		ACE ("Ace", 1), TWO ("2", 2), THREE ("3", 3), FOUR ("4", 4), 
-		FIVE ("5", 5), SIX ("6", 6), SEVEN ("7", 7), EIGHT ("8", 8), 
-		NINE ("9", 9), TEN ("10", 10), JACK ("Jack", 11), 
-		QUEEN ("Queen", 12), KING ("King", 13); 
+		ACE  ("Ace", 1),   TWO ("2",   2),      THREE ("3", 3), FOUR  ("4", 4), 
+		FIVE ("5",   5),   SIX ("6",   6),      SEVEN ("7", 7), EIGHT ("8", 8), 
+		NINE ("9",   9),   TEN ("10", 10), 
+		JACK ("Jack", 11), QUEEN ("Queen", 12), KING ("King", 13); 
 		
 		String name;
 		int value;
@@ -36,12 +67,14 @@ public class Card {
 			return name.substring (0, 1);
 		}
 	}
+	
 	private Rank rank;
-	private String suit;
+	private Suit suit;
 	private boolean faceUp;
-	public Rank [] allRanks = {Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, 
-			Rank.FIVE, Rank.SIX, Rank.SEVEN, Rank.EIGHT, Rank.NINE, 
-			Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING};
+	public Rank [] allRanks = { 
+			Rank.ACE,  Rank.TWO,   Rank.THREE,  Rank.FOUR, Rank.FIVE, 
+			Rank.SIX,  Rank.SEVEN, Rank.EIGHT,  Rank.NINE, Rank.TEN, 
+			Rank.JACK, Rank.QUEEN, Rank.KING };
 	
 	public Card (Rank aRank, String aSuit) {
 		setRank (aRank);
@@ -56,13 +89,29 @@ public class Card {
 		faceUp = true;
 	}
 	
-	public Rank getMatchingRank (int aRank) {
+	public Suit getMatchingSuit (String aSuit) {
+		Suit tSuit = Suit.NO_SUIT;
+		
+		if (Suit.CLUBS.getName ().equals (aSuit)) {
+			tSuit = Suit.CLUBS;
+		} else if (Suit.DIAMONDS.getName ().equals (aSuit)) {
+			tSuit = Suit.DIAMONDS;
+		} else if (Suit.HEARTS.getName ().equals (aSuit)) {
+			tSuit = Suit.HEARTS;
+		} else if (Suit.SPADES.getName ().equals (aSuit)) {
+			tSuit = Suit.SPADES;
+		}
+		
+		return tSuit;
+	}
+	
+	public Rank getMatchingRank (int aRankIndex) {
 		Rank tRank;
 		
-		if ((aRank < MIN_RANK) || (aRank > MAX_RANK)) {
+		if ((aRankIndex < MIN_RANK_INDEX) || (aRankIndex > MAX_RANK_INDEX)) {
 			tRank = Rank.NO_CARD;
 		} else {
-			tRank = allRanks [aRank - 1];
+			tRank = allRanks [aRankIndex - 1];
 		}
 		
 		return tRank;
@@ -72,8 +121,12 @@ public class Card {
 		rank = aRank;
 	}
 	
-	private void setSuit (String aSuit) {
+	private void setSuit (Suit aSuit) {
 		suit = aSuit;
+	}
+	
+	private void setSuit (String aSuit) {
+		setSuit (getMatchingSuit (aSuit));
 	}
 	
 	public Rank getRank () {
@@ -81,7 +134,7 @@ public class Card {
 	}
 	
 	public String getSuit () {
-		return suit;
+		return suit.getName ();
 	}
 	
 	public boolean isFaceUp () {
