7b000650beb0470765e3bcc5536be2f11bc88c34
marksmith <marksmith@new-host-3.home>
2020-07-11 20:01:08 -0400

    Initial "StartNewGameAction" Class extending StartNewRoundAction

---
+++ CardGames/src/cards/actions/StartNewGameAction.java
@@ -0,0 +1,37 @@
+package cards.actions;
+
+import cards.effects.InitiateGameEffect;
+import cards.main.GameManager;
+import cards.utilities.XMLNode;
+
+public class StartNewGameAction extends StartNewRoundAction {
+	public final static String NAME = "Start New Game";
+	
+	public StartNewGameAction () {
+		this (NAME);
+	}
+
+	public StartNewGameAction (String aName) {
+		this (aName, NO_ACTOR);
+	}
+
+	public StartNewGameAction (ActorI aActor) {
+		this (NAME, aActor);
+	}
+	
+	public StartNewGameAction (String aName, ActorI aActor) {
+		super (aName, aActor);
+	}
+
+	public StartNewGameAction (XMLNode aActionNode, GameManager aGameManager) {
+		super (aActionNode, aGameManager);
+		setName (NAME);
+	}
+
+	public void addInitiateGameEffect (ActorI aPlayer, boolean aInitiateGame) {
+		InitiateGameEffect tInitiateGameEffect;
+
+		tInitiateGameEffect = new InitiateGameEffect (aPlayer, aInitiateGame);
+		addEffect (tInitiateGameEffect);
+	}
+}
