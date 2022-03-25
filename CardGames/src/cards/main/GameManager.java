9f468b4dcd788996bdae04742b6a68e3af2bf9b1
Krazick <markjsmith60@gmail.com>
2022-03-25 09:02:25 -0400

    Add getXMLBaseDir method for Heartbeat Class

--- CardGames/src/cards/main/GameManager.java
+++ CardGames/src/cards/main/GameManager.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
 import java.util.Random;
 
 import javax.swing.GroupLayout;
@@ -335,6 +336,15 @@ public class GameManager extends JFrame implements NetworkGameSupport {
 				tException.printStackTrace ();
 			}
 		}
+	}
+	
+	@Override
+	public String getXMLBaseDirectory () {
+		String aBaseDirectory;
+
+		aBaseDirectory = "18XX XML Data" + File.separator;
+		
+		return aBaseDirectory;
 	}
 
 	private void handleNetworkAction (XMLNode tXMLGameActivityNode) {
