adb17259bac96ba61cccccd3ed469627f23ab8be
Krazick <markjsmith60@gmail.com>
2022-08-27 13:49:38 -0400

    Sync up

---
+++ JUnitSource/CardGamesFullTestSuite.java
@@ -0,0 +1,15 @@
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName ("Card Games Package Test Suite")
+@SelectPackages ({ "cards.actions", "cards.config", "cards.effects", "cards.main", 
+					"cards.network", "cards.utilities" })
+
+class CardGamesFullTestSuite {
+
+
+}
