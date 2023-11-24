913e815a64a2602b1d60f7de08a29e9e08d52694
Krazick <markjsmith60@gmail.com>
2023-11-24 13:03:17 -0500

    Update to current IP Address of Server

--- src/cards/network/JGameClient.java
+++ src/cards/network/JGameClient.java
@@ -44,8 +44,8 @@ import cards.utilities.XMLElement;
 public class JGameClient extends XMLFrame {
 	private static final long serialVersionUID = 1L;
 	private static final int DEFAULT_SERVER_PORT = 52000;
-	private static final String DEFAULT_REMOTE_SERVER_IP = "108.31.172.56";
-	private static final String DEFAULT_LOCAL_SERVER_IP = "108.31.172.56";
+	private static final String DEFAULT_REMOTE_SERVER_IP = "71.178.230.211";
+	private static final String DEFAULT_LOCAL_SERVER_IP = "71.178.230.211";
 	private static final String CONNECT_ACTION = "CONNECT";
 	public  static final String EMPTY_GAME_ID = "";
 	public  static final String NO_GAME_ID = "NOID";
